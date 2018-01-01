6c9f37ce3a40f9857d9baafbf9e26699a443458a
Matt Millsaps-Brewer <matt@madebylotus.com>
2018-01-01 10:18:53 -0500

    Provides configuration and filename options

--- lib/capybara/full_screenshot.rb
+++ lib/capybara/full_screenshot.rb
@@ -2,9 +2,30 @@ require "capybara"
 require "launchy"
 
 require "capybara/full_screenshot/version"
+require "capybara/full_screenshot/rspec_helpers"
 
 module Capybara
   module FullScreenshot
-    # Your code goes here...
+    module_function
+
+    def storage_directory
+      return Rails.root.join('tmp', 'capybara') if defined?(Rails)
+
+      File.expand_path('./')
+    end
+
+    def screenshot_path
+      File.join(storage_directory, unique_filename)
+    end
+
+    def unique_filename
+      "full-page-screenshot-#{ Time.now.to_i }.png"
+    end
+  end
+end
+
+if defined?(RSpec)
+  RSpec.configure do |config|
+    config.include Capybara::FullScreenshot::RSpecHelpers, type: :feature
   end
 end
--- spec/capybara/full_screenshot_spec.rb
+++ spec/capybara/full_screenshot_spec.rb
@@ -5,7 +5,46 @@ RSpec.describe Capybara::FullScreenshot do
     expect(Capybara::FullScreenshot::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe '.storage_directory' do
+    context 'when Rails is present' do
+      before do
+        stub_const("Rails", Class.new)
+        allow(Rails).to receive_message_chain(:root, :join).with('tmp', 'capybara').and_return('capybara')
+      end
+
+      it 'returns the result of using Rails root path' do
+        expect(subject.storage_directory).to eq('capybara')
+      end
+    end
+
+    context 'without Rails' do
+      it 'returns absolute current path' do
+        expect(subject.storage_directory).to eq(File.expand_path('./'))
+      end
+    end
+  end
+
+  describe '.screenshot_path' do
+    before do
+      allow(subject).to receive(:unique_filename).and_return('filename.png')
+    end
+
+    it 'returns a String' do
+      expect(subject.screenshot_path).to be_a(String)
+    end
+
+    it 'returns an absolute path' do
+      expect(subject.screenshot_path).to eq(File.expand_path('./filename.png'))
+    end
+  end
+
+  describe '.unique_filename' do
+    before do
+      allow(Time).to receive_message_chain(:now, :to_i).and_return(100)
+    end
+
+    it 'uses the current timestamp in filename' do
+      expect(subject.unique_filename).to eq('full-page-screenshot-100.png')
+    end
   end
 end
